b541a1d16a4c937324887b25482d123fa0dcccfa
shiftcorediv-lab <shiftcore.div@gmail.com>
2026-05-23 03:50:22 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/****************************************************
+ * config.js
+ * OrderCase 共通設定
+ ****************************************************/
+
+window.ORDERCASE_CONFIG = {
+  API_URL: 'https://ordercaseapiproxyworker.shiftcore-div.workers.dev'
+};
