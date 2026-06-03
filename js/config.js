c37c33bded159bd652491fcc44e2ed4d0dde04a7
shiftcorediv-lab <shiftcore.div@gmail.com>
2026-06-04 02:34:32 +0900

    connect ordercase front to firebase id token
    OrderCaseフロントからFirebase IDトークンを取得し、API通信時に自動付与する処理を追加

--- js/config.js
+++ js/config.js
@@ -4,5 +4,16 @@
  ****************************************************/
 
 window.ORDERCASE_CONFIG = {
-  API_URL: 'https://ordercaseapiproxyworker.shiftcore-div.workers.dev'
+  API_URL: 'https://ordercaseapiproxyworker.shiftcore-div.workers.dev',
+
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyAXDhMT1IP1xQ9f0WiOIjmmfBHoQDWZ0dI",
+    authDomain: "shiftcore-div.firebaseapp.com",
+    projectId: "shiftcore-div",
+    storageBucket: "shiftcore-div.firebasestorage.app",
+    messagingSenderId: "882342275588",
+    appId: "1:882342275588:web:bab610608d1bc00453e351"
+  },
+
+  LOGIN_URL: 'https://shiftcorediv-lab.github.io/shiftcore-account-front/'
 };
